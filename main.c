5db0c499b67d25c4f222a2acf043e9fe20028e0b
CimyJr <juniorqueiros45@gmail.com>
2025-02-13 10:08:21 -0300

    refact: removing file functions

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#include "struct_data.c"
+#include "file_handler.c"
 
 #define INSERT_COMMAND "insert into"
 
@@ -84,71 +84,11 @@ void add_client(client **list, const char* name, const char* address, const char
     *list = new_client;
 }
 
-void serialize(client *lista, const char* filename) {
-    FILE* file = fopen(filename, "wb");
-    if(!file) {
-        printf("Não foi possivel abrir o arquivo!");
+void show_list(client* head) {
+    if (!head) {
+        printf("A lista esta vazia!\n");
         return;
     }
-
-    client *current = lista;
-    while(current) {
-        fwrite(current->name, sizeof(char), 50, file);
-        fwrite(current->address, sizeof(char), 100, file);
-        fwrite(&(current->code), sizeof(int), 1, file);
-        fwrite(current->birth, sizeof(char), 20, file);
-        fwrite(current->phone, sizeof(char), 20, file);
-        current = current->next;
-    }
-
-    fclose(file);
-    printf("Linked list saved to %s\n", filename);
-}
-
-client* deserialize(const char* filename) {
-    FILE* file = fopen(filename, "rb");
-    if (!file) {
-        printf("Failed to open file!");
-        return NULL;
-    }
-
-    // A cabeça da fila só será usada uma vez, quando ela for nula,
-    // depois usaremos apenas o ponteiro current para ir ligando os outros valores
-    client *head = NULL;
-    client *current = NULL;
-
-    while(1) {
-        // Por que aqui não foi usado createClient? Porque aqui eu preciso de um "objeto" vazio
-        client *newClient = malloc(sizeof(client));
-        newClient->next = NULL;
-
-        size_t nameReaded = fread(newClient->name, sizeof(char), 50, file);
-        size_t addressReaded = fread(newClient->address, sizeof(char), 100, file);
-        size_t codeReaded = fread(&(newClient->code), sizeof(int), 1, file);
-        size_t birthReaded = fread(newClient->birth, sizeof(char), 20, file);
-        size_t phoneReaded = fread(newClient->phone, sizeof(char), 20, file);
-
-        // Eu queria melhorar esse if :P
-        if(nameReaded == 0 || addressReaded == 0 || codeReaded == 0 || phoneReaded == 0 || birthReaded == 0) {
-            free(newClient);
-            break;
-        }
-// head->primeiro_cliente
-// current -> new_cliente
-        if(!head) {
-            head = newClient;
-            current = head;
-        } else {
-            current->next = (struct client*) newClient;
-            current = (client*) current->next;
-        }
-    }
-
-    fclose(file);
-    return head;
-}
-
-void show_list(client* head) {
     client* aux = head;
     while (aux) {
         printf("Name: %s\nAddress: %s\nCode: %d\nBirth: %s\nPhone: %s", aux->name, aux->address, aux->code, aux->birth, aux->phone);
@@ -166,11 +106,3 @@ int main() {
     free(lista_deserializada);
     return 0;
 }
-
-
-
-
-
-
-
-
