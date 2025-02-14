1520e0a960edf870eab821997c00d0ec7c7cc5a1
CimyJr <juniorqueiros45@gmail.com>
2025-02-14 19:12:03 -0300

    feat: pet type and pet serializers

--- file_handler.c
+++ file_handler.c
@@ -9,15 +9,59 @@ void serialize(client *lista, const char* filename) {
     }
 
     client *current = lista;
-    while(current) {
-        fwrite(current->name, sizeof(char), 50, file);
-        fwrite(current->address, sizeof(char), 100, file);
-        fwrite(&(current->code), sizeof(int), 1, file);
-        fwrite(current->birth, sizeof(char), 20, file);
-        fwrite(current->phone, sizeof(char), 20, file);
-        current = current->next;
+
+        while(current) {
+            fwrite(current->name, sizeof(char), 50, file);
+            fwrite(current->address, sizeof(char), 100, file);
+            fwrite(&(current->code), sizeof(int), 1, file);
+            fwrite(current->birth, sizeof(char), 20, file);
+            fwrite(current->phone, sizeof(char), 20, file);
+            current = current->next;
+        }
+        
+    
+    fclose(file);
+    printf("Linked list saved to %s\n", filename);
+}
+
+void serialize(pet_type *lista, const char* filename) {
+    FILE* file = fopen(filename, "wb");
+    if(!file) {
+        printf("Não foi possivel abrir o arquivo!");
+        return;
     }
 
+    pet_type *current = lista;
+
+        while(current) {
+            fwrite(current->desc, sizeof(char), 100, file);
+            fwrite(&(current->code), sizeof(int), 1, file);
+            current = current->next;
+        }
+        
+    
+    fclose(file);
+    printf("Linked list saved to %s\n", filename);
+}
+
+void serialize(pet *lista, const char* filename) {
+    FILE* file = fopen(filename, "wb");
+    if(!file) {
+        printf("Não foi possivel abrir o arquivo!");
+        return;
+    }
+
+    pet *current = lista;
+
+        while(current) {
+            fwrite(current->name, sizeof(char), 40, file);
+            fwrite(&(current->code), sizeof(int), 1, file);
+            fwrite(&(current->code_client), sizeof(int), 1, file);
+            fwrite(current->pet_type_code, sizeof(char*), 1, file);
+            current = current->next;
+        }
+        
+    
     fclose(file);
     printf("Linked list saved to %s\n", filename);
 }
